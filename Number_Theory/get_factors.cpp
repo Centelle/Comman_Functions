dcb4f31d80866778d97b043ed7378f8a7a83cd3c
Centelle <59483055+Centelle@users.noreply.github.com>
2020-06-03 20:15:04 +0530

    Update get_factors.cpp

--- Number_Theory/get_factors.cpp
+++ Number_Theory/get_factors.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 vector<int> get_factors(int n){
+    /*
+    * returns a sorted vector of all factors of n
+    * factors include 1 and n itself
+    */
     vector<int> f;
     for(int i = 1; i * i <= n; ++i){
         if(n % i == 0){
@@ -15,13 +19,4 @@ vector<int> get_factors(int n){
     }
     sort(f.begin(), f.end());
     return f;
-}
-
-int main(){
-    int n = 100;
-    vector<int> f = get_factors(n);
-    for(auto x : f){
-        cout << x << ' ';
-    }
-    cout << endl;
 }
